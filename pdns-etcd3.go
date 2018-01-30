d988a5fdde269b0758108decff9847a80e34c2b5
nix <nix@>
2018-01-30 08:22:02 +0100

    updated copyright

--- pdns-etcd3.go
+++ pdns-etcd3.go
@@ -1,4 +1,4 @@
-/* Copyright 2016 nix <https://github.com/nixn>
+/* Copyright 2016-2018 nix <https://github.com/nixn>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
