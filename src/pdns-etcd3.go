67c2f3310f5f89fd31002b835c47ce970ec3fccf
nix <nix@>
2025-02-20 20:42:56 +0100

    bumped copyright to 2025

--- src/pdns-etcd3.go
+++ src/pdns-etcd3.go
@@ -1,4 +1,4 @@
-/* Copyright 2016-2024 nix <https://keybase.io/nixn>
+/* Copyright 2016-2025 nix <https://keybase.io/nixn>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -228,7 +228,7 @@ func Main(programVersion VersionType, gitVersion string) {
 	if "v"+releaseVersion != gitVersion {
 		releaseVersion += fmt.Sprintf("[%s]", gitVersion)
 	}
-	log.main().Printf("pdns-etcd3 %s, Copyright © 2016-2024 nix <https://keybase.io/nixn>", releaseVersion)
+	log.main().Printf("pdns-etcd3 %s, Copyright © 2016-2025 nix <https://keybase.io/nixn>", releaseVersion)
 	// handle arguments // TODO handle more arguments, f.e. 'show-defaults' standalone command
 	unixSocketPath := flag.String("unix", "", `Create a unix socket at given path and run in Unix Connector mode ("standalone")`)
 	args = programArgs{
